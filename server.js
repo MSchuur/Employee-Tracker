459682971256e42d40f398efd044be657ed8731d
MSchuur <mschuur31@gmail.com>
2023-04-03 11:10:36 -0300

    Created the required connection for mysql

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employee_db'
+    }
+)
