116beafdf4f8f03d0c31d446f30219fdaf152467
MSchuur <mschuur31@gmail.com>
2023-04-05 11:57:37 -0300

    The schema and seeds load into mysql properly and all data is displayable at the command line

--- schema.sql
+++ schema.sql
@@ -14,16 +14,16 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary INT,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    department INT,
+    FOREIGN KEY (department) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE `employees` (
     id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 )
